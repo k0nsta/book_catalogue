c44d1440369b3ed16ec34504cfdc27b154da2838
KostaNau <naumov.k@gmail.com>
2018-04-26 18:05:23 +0300

    temporary commented unsued file

--- api/permissions.py
+++ api/permissions.py
@@ -1,9 +1,9 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+# from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsAdminOrUserReadOnly(BasePermission):
-    def has_object_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        else:
-            return obj.
+# class IsAdminOrUserReadOnly(BasePermission):
+#     def has_object_permission(self, request, view):
+#         if request.method in SAFE_METHODS:
+#             return True
+#         else:
+#             return obj.
