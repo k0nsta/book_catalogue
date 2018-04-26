3acdb72eec2e18fe28e449eac7d03ad0e24b4385
KostaNau <naumov.k@gmail.com>
2018-04-26 20:32:22 +0300

    typo fix

--- api/permissions.py
+++ api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrUserReadOnly(BasePermission):
     """
-    The request is authenticated as a user, or is a read-only request.
+    The request is authenticated as a admin, or is a user read-only request.
     """
 
     def has_permission(self, request, view):
@@ -13,13 +13,3 @@ class IsAdminOrUserReadOnly(BasePermission):
             request.user.is_staff
         )
 
-
-
-
-# class IsAdminUser(BasePermission):
-#     """
-#     Allows access only to admin users.
-#     """
-
-#     def has_permission(self, request, view):
-#         return request.user and request.user.is_staff
